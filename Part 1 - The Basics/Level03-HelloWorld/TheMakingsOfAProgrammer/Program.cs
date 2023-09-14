c5af3aed1e40629380f4bd16693f23bda1b642d7
cjmedina95 <Christian.Medina.ATX@gmail.com>
2023-09-14 09:09:58 -0700

    Initial commit

---
+++ Part 1 - The Basics/Level03-HelloWorld/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("According to all known laws");
+Console.WriteLine("of aviation,");
+Console.WriteLine("");
+Console.WriteLine("there is no way a bee");
+Console.WriteLine("should be able to fly.");
